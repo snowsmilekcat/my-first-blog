db210b93ea514a713c415fdba8756c074af3d89d
Your Name <you@example.com>
2019-12-01 17:18:24 +0900

    2019/12/01 Test

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path#パス関数のインポート
 from . import views#アプリファイル内の全てのビューをインポートする
 
 urlpatterns = [
-  path('',views.post_list,name='post_list'),#post_listという名前のビューをルートURLに割り当て
+  path('',views.post_list,name='post_list'),#views.pyというビューファイル内のpost_listという関数をルートURLに割り当て
 ]
