547f878120f5c8869d9eb81b299486e868e78462
Your Name <you@example.com>
2019-12-01 18:35:03 +0900

    2019/12/01 Python_HLTM Test

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path#パス関数のインポート
 from . import views#アプリファイル内の全てのビューをインポートする
 
 urlpatterns = [
-  path('',views.post_list,name='post_list'),#views.pyというビューファイル内のpost_listという関数をルートURLに割り当て
+  path('',views.post_list,name='post_list')#views.pyというビューファイル内のpost_listという名前の関数をルートURLに割り当て
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post#modelsファイル内のPostクラスの参照
 
 # Create your views here.
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')#Postモデルより、データの取り出し。
+    return render(request,'blog/post_list.html',{'posts': posts})
