b721485f17c43631306aa11cf44ccfa66f6f0c2d
Your Name <you@example.com>
2019-12-08 17:31:54 +0900

    2019/12/08 PythonTest テンプレート作成

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path#パス関数のインポート
 from . import views#アプリファイル内の全てのビューをインポートする
 
 urlpatterns = [
-  path('',views.post_list,name='post_list')#views.pyというビューファイル内のpost_listという名前の関数をルートURLに割り当て
+  path('',views.post_list,name='post_list'),#views.pyというビューファイル内のpost_listという名前の関数をルートURLに割り当て
+  path('post/<int:pk>/', views.post_detail, name='post_detail'),#<int:pk>の部分は、int型のpkという変数に対応した値がパスとして続くことを要求するための記載。
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post#modelsファイル内のPostクラスの参照
+from django.shortcuts import render, get_object_or_404#エラー画面表示のためのインポート
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')#Postモデルより、データの取り出し。
     return render(request,'blog/post_list.html',{'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
