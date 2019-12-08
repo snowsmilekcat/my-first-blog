ce682dc4a1df692d10cffdae65d30988724126c6
Your Name <you@example.com>
2019-12-08 19:03:20 +0900

    2019/12/08 新規・修正テスト

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views#アプリファイル内の全てのビューをインポー
 urlpatterns = [
   path('',views.post_list,name='post_list'),#views.pyというビューファイル内のpost_listという名前の関数をルートURLに割り当て
   path('post/<int:pk>/', views.post_detail, name='post_detail'),#<int:pk>の部分は、int型のpkという変数に対応した値がパスとして続くことを要求するための記載。
+  path('post/new/', views.post_new, name='post_new'),
+  path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post#modelsファイル内のPostクラスの参照
 from django.shortcuts import render, get_object_or_404#エラー画面表示のためのインポート
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -11,3 +13,34 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_new(request):
+    if request.method == "POST":#リクエストがPOSTかどうかで制御を分岐
+        form = PostForm(request.POST)
+        if form.is_valid():#画面のチェック処理で問題ないかで分岐
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()#更新内容をデータベースに反映
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):#編集のため、今保有しているpkも取得する。
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
